855145a98a17e8fc9bf136037f90e2150d2dc3a6
Jossie Zamora <JZ574@njit.edu>
2021-02-03 20:16:13 -0500

    Addition of README.txt

--- app/src/main/java/com/example/flixster20/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster20/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster20.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,12 +31,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     @Override
 
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(movieView);
     }
     //populating data
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder "+ position);
         Movie movie = movies.get(position);
         holder.bind(movie);
     }
